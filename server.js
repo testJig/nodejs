c955956cbb294d2ad6b355e04fc52ec4deab83fd
Your Name <you@example.com>
2018-09-10 18:29:15 +0530

    sss
    Signed-off-by: Your Name <you@example.com>

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express =require('express');
 var app = express();
-
-//var server = require('http').createServer(app);
-//var io = require('socket.io')(server);
-// app.set('port', process.env.PORT || 3000);
-// var server = app.listen(app.get('port'), function () {
-    // console.log('Express server listening on port ' + server.address().port);
-// });
-// io = require('socket.io')(server);
-
 var port=process.env.PORT || 3000;
 
- app.use(express.static(__dirname + '/public'));
- console.log('server listening at port'+port);
+app.use(express.static(__dirname + '/public'));
 
- app.get('/',function(req,res){
- console.log('hello from server');
-  res.render('./public/nilesh.html');
+
+app.get('/',function(req,res){
+console.log('hello from server');
+ res.render('./public/index.html');
 });
 
 app.listen(port);
-console.log('server listening at port'+port);
+console.log('Server Listening at port'+port);
