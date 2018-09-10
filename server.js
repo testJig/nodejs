6b68c0cacf717fbe4c92dc5faea8b2085d8758a5
Your Name <you@example.com>
2018-09-10 16:29:01 +0530

    New1

--- server.js
+++ server.js
@@ -1,44 +1,14 @@
-
-
 var express =require('express');
 var app = express();
-let http = require('https').Server(app);
-let io = require('socket.io')(http);
 var port=process.env.PORT || 3000;
 
-app.get(/^socket.io.js$/, function(req, res) {
+app.use(express.static(__dirname + '/public'));
+console.log('Server Listening at port'+port);
 
-  res.setHeader('Access-Control-Allow-Origin', 'TRUE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-  res.sendfile('socket.io.js');
-  next();
-  
-  });
-
-
-// app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // next();
-// });
-
-io.on('connection', (socket) => {
-  console.log('user connected');
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
-  socket.on('add-message', (message) => {
-    io.emit('message', {type:'new-message', text: message});    
-  });
+app.get('/',function(req,res){
+console.log('hello from server');
+ res.render('./public/nilesh.html');
 });
 
-http.listen(port, () => {
-  console.log(http);
-});
-
-
-
+app.listen(port);
+console.log('Server Listening at port'+port);
