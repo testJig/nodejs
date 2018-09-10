b705e107defc563118e9042dda0aa994530f0633
Your Name <you@example.com>
2018-09-10 17:22:13 +0530

    h

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express =require('express');
 var app = express();
 
-var server = require('http').createServer(app);
+//var server = require('http').createServer(app);
+//var io = require('socket.io')(server);
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
 var io = require('socket.io')(server);
-
 
 var port=process.env.PORT || 3000;
 
@@ -15,5 +19,5 @@ console.log('hello from server');
  res.render('./public/nilesh.html');
 });
 
-app.listen(port);
+//app.listen(port);
 console.log('Server Listening at port'+port);
