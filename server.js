8b8cb278db42671a6413c6c07199728f206c9d27
Your Name <you@example.com>
2018-09-10 15:12:28 +0530

    mnnbm

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express =require('express');
 var app = express();
-var port=process.env.PORT || 3000;
+var port=3000;
 
 app.use(express.static(__dirname + '/public'));
-
+console.log('Server Listening at port'+port);
 
 app.get('/',function(req,res){
 console.log('hello from server');
- res.render('./public/nilesh.html');
+ res.render('./public/nilesh.html'); //fg
 });
 
 app.listen(port);
