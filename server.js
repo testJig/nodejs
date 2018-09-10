63346ab2dc124b87138e8365cfad022459968444
Your Name <you@example.com>
2018-09-10 17:14:27 +0530

    u

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express =require('express');
 var app = express();
 
-var server = require('https').createServer(app);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 
